99b14910f1895d9c5155ba5c791359dc5ba50e83
Scooter <92922224+scooterthedev@users.noreply.github.com>
2026-02-12 17:46:45 +0000

    works now?

--- config/initializers/flipper.rb
+++ config/initializers/flipper.rb
@@ -3,6 +3,8 @@
 # Flipper is configured automatically with the ActiveRecord adapter
 # when flipper-active_record gem is loaded
 
+require 'flipper/adapters/active_record'
+
 # Ensure access flipper feature exists and is enabled globally by default
 # This allows all existing users to continue accessing the app
 Rails.application.config.after_initialize do
