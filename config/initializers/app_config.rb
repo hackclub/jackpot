9d2cfba20071eda647c7998db413cc62d9937197
Neon <neon@saahild.com>
2026-03-12 17:02:15 -0400

    fix: um idk anymore

--- config/initializers/app_config.rb
+++ config/initializers/app_config.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 # Load configuration from Rails credentials (falls back to env vars for CI/test)
-if Rails.application.credentials.config.key_path.exist?
+begin
   Rails.application.config.x.hack_club.client_id = Rails.application.credentials.dig(:hack_club, :client_id)
   Rails.application.config.x.hack_club.client_secret = Rails.application.credentials.dig(:hack_club, :client_secret)
   Rails.application.config.x.lockbox.master_key = Rails.application.credentials.lockbox_master_key
-else
+rescue ArgumentError
   Rails.application.config.x.hack_club.client_id = ENV["HACK_CLUB_CLIENT_ID"]
   Rails.application.config.x.hack_club.client_secret = ENV["HACK_CLUB_CLIENT_SECRET"]
   Rails.application.config.x.lockbox.master_key = ENV["LOCKBOX_MASTER_KEY"]
