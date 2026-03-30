a40244d6f8b62b48ddf49ad1ad2c7d467c87f640
Memma <132008224+MeMMa137@users.noreply.github.com>
2026-03-30 11:08:03 -0400

    FIX - db test pr fail

--- test/models/jackpot_hours_test.rb
+++ test/models/jackpot_hours_test.rb
@@ -9,9 +9,9 @@ class JackpotHoursTest < ActiveSupport::TestCase
     assert_in_delta 11.59, JackpotHours.hackatime_hours_from_api_total(11.599), 1e-9
   end
 
-  test "chips_from_approved_hours never rounds chip delta up" do
+  test "chips_from_approved_hours floors chip amount at cent precision (hours × 50, never rounds up)" do
     assert_in_delta 575.0, JackpotHours.chips_from_approved_hours(11.5), 1e-9
-    assert_in_delta 595.0, JackpotHours.chips_from_approved_hours(11.99), 1e-9
+    assert_in_delta 599.5, JackpotHours.chips_from_approved_hours(11.99), 1e-9
     assert_in_delta 25.0, JackpotHours.chips_from_approved_hours(0.5), 1e-9
   end
 end
